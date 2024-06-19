ea963e0a113d3a9a8f61f6bf253af6b7a84e53be
kony <2312708932@qq.com>
2024-06-19 17:08:01 +0800

    u

--- proxy/proxy_l.go
+++ proxy/proxy_l.go
@@ -3,41 +3,11 @@ package proxy
 import (
 	"context"
 	"goodlink/tools"
-	"io"
 	"log"
 	"net"
-	"time"
 
 	"github.com/quic-go/quic-go"
 )
-
-func stunT2QProcess1(tc net.Conn, qc quic.Stream, stun_quic_conn quic.Connection) {
-	for {
-		tc.SetDeadline(time.Now().Add(15 * time.Minute))
-		qc.SetDeadline(time.Now().Add(15 * time.Minute))
-		if n, err := io.Copy(tc, qc); n == 0 || err != nil {
-			log.Printf("stunT2QProcess1 tcp close: %v==>%v\n", tc.RemoteAddr(), tc.LocalAddr())
-			tc.Close()
-			log.Printf("stunT2QProcess1 quic stream close: %v, %v==>%v\n", qc.StreamID(), stun_quic_conn.RemoteAddr(), stun_quic_conn.LocalAddr())
-			qc.Close()
-			break
-		}
-	}
-}
-
-func stunQ2TProcess1(qc quic.Stream, tc net.Conn, stun_quic_conn quic.Connection) {
-	for {
-		tc.SetDeadline(time.Now().Add(15 * time.Minute))
-		qc.SetDeadline(time.Now().Add(15 * time.Minute))
-		if n, err := io.Copy(qc, tc); n == 0 || err != nil {
-			log.Printf("stunQ2TProcess1 tcp close: %v==>%v\n", tc.RemoteAddr(), tc.LocalAddr())
-			tc.Close()
-			log.Printf("stunQ2TProcess1 quic stream close: %v, %v==>%v\n", qc.StreamID(), stun_quic_conn.RemoteAddr(), stun_quic_conn.LocalAddr())
-			qc.Close()
-			break
-		}
-	}
-}
 
 func ProcessProxyClient(addr string, stun_quic_conn quic.Connection) {
 	log.Println("ProcessProxyClient start...")
