50fa5e9a5edd4ed29ffe5c44c828ef85da03ea07
kony <2312708932@qq.com>
2024-12-21 15:55:15 +0800

    删除部分日志输出

--- proxy/proxy_l.go
+++ proxy/proxy_l.go
@@ -14,19 +14,13 @@ func ProcessProxyClient(listener net.Listener, stun_quic_conn quic.Connection) {
 	for {
 		new_tcp_conn, err := listener.Accept()
 		if err == nil {
-			//log.Printf("   ProcessProxyClient listener.Accept: %v ==> %v\n", new_tcp_conn.RemoteAddr(), new_tcp_conn.LocalAddr())
 			new_quic_stream, err := stun_quic_conn.OpenStreamSync(context.Background())
 			if err == nil {
-				//log.Printf("   ProcessProxyClient stun_quic_conn.OpenStreamSync: %v ==> %v\n", stun_quic_conn.RemoteAddr(), stun_quic_conn.LocalAddr())
 				go stunT2QProcess1(new_tcp_conn, new_quic_stream, stun_quic_conn)
 				go stunQ2TProcess1(new_quic_stream, new_tcp_conn, stun_quic_conn)
 				continue
-			} else {
-				log.Printf("   ProcessProxyClient stun_quic_conn.OpenStreamSync: %v\n", err)
 			}
 			break
-		} else {
-			log.Printf("   ProcessProxyClient listener.Accept: %v\n", err)
 		}
 		break
 	}
--- proxy/proxy_r.go
+++ proxy/proxy_r.go
@@ -19,18 +19,13 @@ func ProcessProxyServer(addr string, stun_quic_conn quic.Connection) {
 	for {
 		new_quic_stream, err := stun_quic_conn.AcceptStream(context.Background())
 		if err == nil {
-			//log.Printf("   ProcessProxyServer new_quic_conn.AcceptStream: %v ==> %v\n", stun_quic_conn.RemoteAddr(), stun_quic_conn.LocalAddr())
 			new_tcp_conn, err := net.Dial("tcp", addr)
 			if err == nil {
 				go stunT2QProcess1(new_tcp_conn, new_quic_stream, stun_quic_conn)
 				go stunQ2TProcess1(new_quic_stream, new_tcp_conn, stun_quic_conn)
 				continue
-			} else {
-				log.Printf("   ProcessProxyServer net.Dial: %v\n", err)
 			}
 			break
-		} else {
-			log.Printf("   ProcessProxyServer stun_quic_conn.AcceptStream: %v\n", err)
 		}
 		break
 	}
