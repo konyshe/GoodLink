2cdda6096f93111ef668c111d02184135266b5eb
kony <2312708932@qq.com>
2024-12-24 19:26:55 +0800

    u

--- proxy/proxy_io.go
+++ proxy/proxy_io.go
@@ -11,9 +11,11 @@ import (
 func stunT2QProcess1(tc net.Conn, qc quic.Stream, stun_quic_conn quic.Connection) {
 	for {
 		if n, err := io.Copy(tc, qc); n == 0 || err != nil {
-			log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
 			tc.Close()
 			qc.Close()
+			if err != nil {
+				log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
+			}
 			break
 		}
 	}
@@ -22,9 +24,11 @@ func stunT2QProcess1(tc net.Conn, qc quic.Stream, stun_quic_conn quic.Connection
 func stunQ2TProcess1(qc quic.Stream, tc net.Conn, stun_quic_conn quic.Connection) {
 	for {
 		if n, err := io.Copy(qc, tc); n == 0 || err != nil {
-			log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
 			tc.Close()
 			qc.Close()
+			if err != nil {
+				log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
+			}
 			break
 		}
 	}
