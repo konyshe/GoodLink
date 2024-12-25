11345257c0e844a839979eafa2eeeb695d74c1d5
kony <2312708932@qq.com>
2024-12-25 09:24:11 +0800

    过滤无需关注的错误

--- proxy/proxy_io.go
+++ proxy/proxy_io.go
@@ -4,16 +4,26 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 
 	"github.com/quic-go/quic-go"
 )
+
+func check(err error) bool {
+	if err != nil {
+		if !strings.Contains(err.Error(), "time out") && !strings.Contains(err.Error(), "closed") {
+			return true
+		}
+	}
+	return false
+}
 
 func stunT2QProcess1(tc net.Conn, qc quic.Stream, stun_quic_conn quic.Connection) {
 	for {
 		if n, err := io.Copy(tc, qc); n == 0 || err != nil {
 			tc.Close()
 			qc.Close()
-			if err != nil {
+			if check(err) {
 				log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
 			}
 			break
@@ -26,7 +36,7 @@ func stunQ2TProcess1(qc quic.Stream, tc net.Conn, stun_quic_conn quic.Connection
 		if n, err := io.Copy(qc, tc); n == 0 || err != nil {
 			tc.Close()
 			qc.Close()
-			if err != nil {
+			if check(err) {
 				log.Printf("udp.conn: %v, quic.conn: %v, quic.stream: %v, err: %v\n", tc.RemoteAddr(), stun_quic_conn.RemoteAddr(), qc.StreamID(), err)
 			}
 			break
