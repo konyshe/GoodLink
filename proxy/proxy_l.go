5b0f107c3c57ef536b3a7ed4f0fb0c881a50218b
kony <2312708932@qq.com>
2025-01-22 15:17:39 +0800

    调整状态显示

--- proxy/proxy_l.go
+++ proxy/proxy_l.go
@@ -2,14 +2,14 @@ package proxy
 
 import (
 	"context"
-	"goodlink/utils"
+	"log"
 	"net"
 
 	"github.com/quic-go/quic-go"
 )
 
 func ProcessProxyClient(listener net.Listener, stun_quic_conn quic.Connection) {
-	utils.Log().Debug("您已可以访问remote端的主机, 请勿关闭本程序")
+	log.Println("您已可以访问remote端的主机, 请勿关闭本程序")
 
 	for {
 		new_tcp_conn, err := listener.Accept()
