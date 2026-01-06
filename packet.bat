3bdd220cd7918c0e9e093d5f5d7ef3715511f79a
kony <2312708932@qq.com>
2026-01-06 09:14:31 +0800

    Update packet.bat to remove temporary command files after zipping and eliminate unused MIPS and RISC-V zip commands for a cleaner build process.

--- packet.bat
+++ packet.bat
@@ -11,13 +11,19 @@ make clean
 make linux
 cd bin
 zip goodlink-linux-amd64-cmd.zip goodlink-linux-amd64-cmd
+rm -rf goodlink-linux-amd64-cmd
+
 zip goodlink-linux-arm64-cmd.zip goodlink-linux-arm64-cmd
+rm -rf goodlink-linux-arm64-cmd
+
 zip goodlink-linux-386-cmd.zip goodlink-linux-386-cmd
+rm -rf goodlink-linux-386-cmd
+
 zip goodlink-linux-arm-cmd.zip goodlink-linux-arm-cmd
+rm -rf goodlink-linux-arm-cmd
+
 zip goodlink-linux-armv6l-cmd.zip goodlink-linux-armv6l-cmd
+rm -rf goodlink-linux-armv6l-cmd
+
 zip goodlink-linux-loong64-cmd.zip goodlink-linux-loong64-cmd
-zip goodlink-linux-mips-cmd.zip goodlink-linux-mips-cmd
-zip goodlink-linux-mipsle-cmd.zip goodlink-linux-mipsle-cmd
-zip goodlink-linux-mips64-cmd.zip goodlink-linux-mips64-cmd
-zip goodlink-linux-mips64le-cmd.zip goodlink-linux-mips64le-cmd
-zip goodlink-linux-riscv64-cmd.zip goodlink-linux-riscv64-cmd
+rm -rf goodlink-linux-loong64-cmd
