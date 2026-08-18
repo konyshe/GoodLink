d8a7db2a404a7656b32f0021476c180279684fba
kony <2312708932@qq.com>
2026-08-18 07:13:57 +0000

    build(release): 压缩后移除各平台构建产物
    在生成 Linux 与 macOS 发布包后立即清理对应二进制文件，
    减少打包目录中的冗余产物并避免后续流程受到残留文件影响。

--- packet.sh
+++ packet.sh
@@ -16,17 +16,16 @@ make macos
 cd bin
 
 mv ../goodlink-windows-amd64.zip .
-zip goodlink-linux-amd64.zip goodlink-linux-amd64
-zip goodlink-linux-arm64.zip goodlink-linux-arm64
-zip goodlink-linux-386.zip goodlink-linux-386
-zip goodlink-linux-arm.zip goodlink-linux-arm
-zip goodlink-linux-armv6l.zip goodlink-linux-armv6l
-zip goodlink-linux-loong64.zip goodlink-linux-loong64
-zip goodlink-linux-mips.zip goodlink-linux-mips
-zip goodlink-linux-mipsle.zip goodlink-linux-mipsle
-zip goodlink-linux-mips64.zip goodlink-linux-mips64
-zip goodlink-linux-mips64le.zip goodlink-linux-mips64le
-zip goodlink-linux-riscv64.zip goodlink-linux-riscv64
-
-zip goodlink-darwin-amd64.zip goodlink-darwin-amd64
-zip goodlink-darwin-arm64.zip goodlink-darwin-arm64
+zip goodlink-linux-amd64.zip goodlink-linux-amd64; rm -rf goodlink-linux-amd64
+zip goodlink-linux-arm64.zip goodlink-linux-arm64; rm -rf goodlink-linux-arm64
+zip goodlink-linux-386.zip goodlink-linux-386; rm -rf goodlink-linux-386
+zip goodlink-linux-arm.zip goodlink-linux-arm; rm -rf goodlink-linux-arm
+zip goodlink-linux-armv6l.zip goodlink-linux-armv6l; rm -rf goodlink-linux-armv6l
+zip goodlink-linux-loong64.zip goodlink-linux-loong64; rm -rf goodlink-linux-loong64
+zip goodlink-linux-mips.zip goodlink-linux-mips; rm -rf goodlink-linux-mips
+zip goodlink-linux-mipsle.zip goodlink-linux-mipsle; rm -rf goodlink-linux-mipsle
+zip goodlink-linux-mips64.zip goodlink-linux-mips64; rm -rf goodlink-linux-mips64
+zip goodlink-linux-mips64le.zip goodlink-linux-mips64le; rm -rf goodlink-linux-mips64le
+zip goodlink-linux-riscv64.zip goodlink-linux-riscv64; rm -rf goodlink-linux-riscv64
+zip goodlink-darwin-amd64.zip goodlink-darwin-amd64; rm -rf goodlink-darwin-amd64
+zip goodlink-darwin-arm64.zip goodlink-darwin-arm64; rm -rf goodlink-darwin-arm64
