639e14aed9f81904685a44976c8137163f2cb7e6
kony <2312708932@qq.com>
2026-08-26 13:43:16 +0800

    chore: 添加同步和挂载命令以确保文件系统一致性

--- packet.sh
+++ packet.sh
@@ -82,4 +82,6 @@ md5sum goodlink-linux-riscv64 > md5sum.txt; zip goodlink-linux-riscv64.zip goodl
 md5sum goodlink-darwin-amd64 > md5sum.txt; zip goodlink-darwin-amd64.zip goodlink-darwin-amd64 md5sum.txt; rm -rf goodlink-darwin-amd64 md5sum.txt
 md5sum goodlink-darwin-arm64 > md5sum.txt; zip goodlink-darwin-arm64.zip goodlink-darwin-arm64 md5sum.txt; rm -rf goodlink-darwin-arm64 md5sum.txt
 
+sleep 1; sync; sync; sync
+mount -a
 rm -rf /mnt/windows/packet; mv bin /mnt/windows/packet
